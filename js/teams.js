2cd49e4013875b77a881088c22ebca04e9e07a84
Kirti Aggarwal <kirti057btcse21@igdtuw.ac.in>
2022-09-03 05:01:23 -0700

    teams added

---
+++ js/teams.js
@@ -0,0 +1,202 @@
+$(window).scroll(function(){
+    if(screen.availWidth>=1024)
+    {
+    for(var i=0;i<3;i++)
+    {    var h = 90+(650*i);
+        var h1=400+(650*i);
+        var pos = $(window).scrollTop();
+        console.log(pos);
+        
+        if(pos > h&&pos<h1){
+          $('.card'+(i+1)).addClass('zoom');
+        }
+        
+        else  {
+          $('.card'+(i+1)).removeClass('zoom');
+    
+        }}
+        for(var i=3,j=3;i<24;i+=2,j++)
+        {    var h = 90+(600*j);
+            var h1=400+(600*j);
+            var pos = $(window).scrollTop();
+            console.log(pos);
+            
+            if(pos > h&&pos<h1){
+              $('.card'+(i+1)).addClass('zoom');
+              $('.card'+(i+2)).addClass('zoom');
+            }
+            
+            else  {
+              $('.card'+(i+1)).removeClass('zoom');
+              $('.card'+(i+2)).removeClass('zoom');
+        
+            }}
+            var h =7757;
+            var h1=8090;
+            var pos = $(window).scrollTop();
+            console.log(pos);
+            
+            if(pos > h&&pos<h1){
+              $('.card22').addClass('zoom');
+              $('.card23').addClass('zoom');
+              $('.card24').addClass('zoom');
+            }
+            else {
+              $('.card22').removeClass('zoom');
+              $('.card23').removeClass('zoom');
+              $('.card24').removeClass('zoom');
+        
+            }
+  
+}
+else{for(var i=0;i<3;i++)
+    {    var h = 90+(600*i);
+        var h1=400+(600*i);
+        var pos = $(window).scrollTop();
+        console.log(pos);
+        
+        if(pos > h&&pos<h1){
+          $('.card'+(i+1)).addClass('zoom');
+        }
+        
+        else  {
+          $('.card'+(i+1)).removeClass('zoom');
+    
+        }}
+        for(var i=3,j=3;i<24;i+=2,j++)
+        {   var h = 90+(650*j);
+          
+            var h1=400+(650*j);
+        
+            var pos = $(window).scrollTop();
+            console.log(pos);
+            
+            if(pos > h&&pos<h1){
+              $('.card'+(i+1)).addClass('zoom');
+              $('.card'+(i+2)).addClass('zoom');
+            }
+            
+            else  {
+              $('.card'+(i+1)).removeClass('zoom');
+              $('.card'+(i+2)).removeClass('zoom');
+        
+            }}
+            var h =5170;
+            var h1=5611;
+            var pos = $(window).scrollTop();
+            console.log(pos);
+            
+            if(pos > h&&pos<h1){
+              $('.card10').addClass('zoom');
+              $('.card11').addClass('zoom');
+            }
+            else {
+              $('.card10').removeClass('zoom');
+              $('.card11').removeClass('zoom');
+        
+            }
+            var h =6249;
+            var h1=6703;
+            var pos = $(window).scrollTop();
+            console.log(pos);
+            
+            if(pos > h&&pos<h1){
+              $('.card12').addClass('zoom');
+              $('.card13').addClass('zoom');
+            }
+            else {
+              $('.card12').removeClass('zoom');
+              $('.card13').removeClass('zoom');
+        
+            }
+            var h =7356;
+            var h1=7704;
+            var pos = $(window).scrollTop();
+            console.log(pos);
+            
+            if(pos > h&&pos<h1){
+              $('.card14').addClass('zoom');
+              $('.card15').addClass('zoom');
+            }
+            else {
+              $('.card14').removeClass('zoom');
+              $('.card15').removeClass('zoom');
+        
+            }
+            var h =8403;
+            var h1=8823;
+            var pos = $(window).scrollTop();
+            console.log(pos);
+            
+            if(pos > h&&pos<h1){
+              $('.card16').addClass('zoom');
+              $('.card17').addClass('zoom');
+            }
+            else {
+              $('.card16').removeClass('zoom');
+              $('.card17').removeClass('zoom');
+        
+            }
+            var h =9526;
+            var h1=9929;
+            var pos = $(window).scrollTop();
+            console.log(pos);
+            
+            if(pos > h&&pos<h1){
+              $('.card18').addClass('zoom');
+              $('.card19').addClass('zoom');
+            }
+            else {
+              $('.card18').removeClass('zoom');
+              $('.card19').removeClass('zoom');
+        
+            }
+            var h =10588;
+            var h1=11039;
+            var pos = $(window).scrollTop();
+            console.log(pos);
+            
+            if(pos > h&&pos<h1){
+              $('.card20').addClass('zoom');
+              $('.card21').addClass('zoom');
+            }
+            else {
+              $('.card20').removeClass('zoom');
+              $('.card21').removeClass('zoom');
+        
+            }
+            var h =11765;
+            var h1=12335;
+            var pos = $(window).scrollTop();
+            console.log(pos);
+            
+            if(pos > h&&pos<h1){
+              $('.card22').addClass('zoom');
+              $('.card23').addClass('zoom');
+              $('.card24').addClass('zoom');
+            }
+            else {
+              $('.card22').removeClass('zoom');
+              $('.card23').removeClass('zoom');
+              $('.card24').removeClass('zoom');
+        
+            }
+            var h =13327;
+            var h1=13881;
+            var pos = $(window).scrollTop();
+            console.log(pos);
+            
+            if(pos > h&&pos<h1){
+              $('.card25').addClass('zoom');
+              $('.card26').addClass('zoom');
+              $('.card27').addClass('zoom');
+            }
+            else {
+              $('.card25').removeClass('zoom');
+              $('.card26').removeClass('zoom');
+              $('.card27').removeClass('zoom');
+        
+            }
+
+}
+});
